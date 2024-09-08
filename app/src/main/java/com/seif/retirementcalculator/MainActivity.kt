aabe745f3bb575a7fba3363f1b76f0e3237e44cb
Seif <seifeldinh@banquemisr.com>
2024-09-08 10:59:28 +0300

    adding app center key and dependencies for analytics and crashes

---
+++ app/src/main/java/com/seif/retirementcalculator/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.seif.retirementcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.seif.retirementcalculator.Constants.Companion.APP_SECRET_KEY
+import com.seif.retirementcalculator.ui.theme.RetirementCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppCenter.start(application, APP_SECRET_KEY, Analytics::class.java, Crashes::class.java)
+            RetirementCalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RetirementCalculatorTheme {
+        Greeting("Android")
+    }
+}
